004461ad402b5e6757c0b68dc853e77360230582
Joji <freedive.cebu30@gmail.com>
2022-06-02 23:16:06 +0800

    Create question_4.ts

---
+++ question_4.ts
@@ -0,0 +1,15 @@
+modulus(5)
+modulus(7)
+modulus(26)
+
+function modulus(num: number){
+   if (num > 25) {
+     console.log(num)
+   } else {
+     for (let $i = 1; $i <= 25; $i++) {
+       if ($i % num == 0) {
+         console.log($i)
+       }
+     }
+   }
+}
