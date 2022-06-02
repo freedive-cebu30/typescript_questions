0f5fed9b2bb568e4cdd38d1d47aacdf64f64cff4
Joji <freedive.cebu30@gmail.com>
2022-06-02 23:02:24 +0800

    Create question_1.ts

---
+++ question_1.ts
@@ -0,0 +1,12 @@
+let number_1: number = 10;
+let number_2: number = 3;
+let number_3: number = 2;
+let number_4: number = 10_000;
+  
+console.log(`${number_1} + ${number_2} = ${number_1 + number_2}`);
+console.log(`${number_1} - ${number_2} = ${number_1 - number_2}`);
+console.log(`${number_1} * ${number_2} = ${number_1 * number_2}`);
+console.log(`${number_4} / ${number_1} = ${number_4 / number_1}`);
+console.log(`${number_1} / ${number_2} = ${number_1 / number_2} 余り ${number_1 % number_2}`);
+console.log(`${number_3}の2乗 = ${number_3 ** 2}`);
+console.log(`${number_3}の3乗 = ${number_3 ** 3}`);
