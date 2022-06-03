10fca70a1aca3aba94f03504417d4e201e257bbf
Joji <freedive.cebu30@gmail.com>
2022-06-03 15:06:10 +0800

    Create question_9.ts

---
+++ question_9.ts
@@ -0,0 +1,18 @@
+// どちらのメソッドでも大丈夫です
+// Either method is fine
+
+console.log(bmi(187, 61));
+console.log(bmi2(187, 61));
+// 17.444021847922443
+// 17.444021847922446
+
+
+function bmi(height: number, weight: number): number {
+  let mHeight = (height / 100) ** 2;
+  
+  return weight / mHeight;
+}
+
+function bmi2(height: number, weight: number): number {
+  return weight * 10000 / height ** 2;
+}
