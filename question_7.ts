d18898426bcfa0ecb3ce47b80132cff59cbd689e
Joji <freedive.cebu30@gmail.com>
2022-06-03 13:50:35 +0800

    Update question_7.ts

--- question_7.ts
+++ question_7.ts
@@ -1,8 +1,8 @@
-showSeason(3);
-showSeason(6);
-showSeason(9);
-showSeason(12);
-showSeason(13);
+showSeason(3)
+showSeason(6)
+showSeason(9)
+showSeason(12)
+showSeason(13)
 
 
 function showSeason(month: number): void {
@@ -10,24 +10,24 @@ function showSeason(month: number): void {
     case 1:
     case 2:
     case 12:
-      console.log('冬');
-      break;
+      console.log('冬')
+      break
     case 3:
     case 4:
     case 5:
-      console.log('春');
-      break;
+      console.log('春')
+      break
     case 6:
     case 7:
     case 8:
-      console.log('夏');
-      break;
+      console.log('夏')
+      break
     case 9:
     case 10:
     case 11:
-      console.log('秋');
-      break;
+      console.log('秋')
+      break
     default:
-      console.log('季節不明');
+      console.log('季節不明')
   }  
 }
