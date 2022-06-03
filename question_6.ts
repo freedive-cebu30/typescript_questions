ad8a65fc761fa63732d9e8386c4b825a4a922842
Joji <freedive.cebu30@gmail.com>
2022-06-03 13:46:18 +0800

    Create question_6.ts

---
+++ question_6.ts
@@ -0,0 +1,25 @@
+let message: string
+
+message = fizzBuzz(6)
+console.log(message)
+message = fizzBuzz(10)
+console.log(message)
+message = fizzBuzz(15)
+console.log(message)
+message = fizzBuzz(16)
+console.log(message)
+
+
+function fizzBuzz(n: number) {
+  let message  = 'Nothing'
+  
+  if (n % 15 == 0) {
+    message = 'FizzBuzz'
+  } else if ( n % 5 == 0 ) {
+    message = 'Buzz'
+  } else if ( n % 3 == 0) {
+    message = 'Fizz'
+  }
+    
+  return message
+}
