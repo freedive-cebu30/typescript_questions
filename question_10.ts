59e445d89d7449640d40ba8d9c5c8f6641987714
Joji <freedive.cebu30@gmail.com>
2022-06-03 15:14:30 +0800

    Update question_10.ts

--- question_10.ts
+++ question_10.ts
@@ -1,11 +1,11 @@
-console.log(cal(3));
-console.log(cal(4));
+console.log(cal(3))
+console.log(cal(4))
 
 
 function cal(num: number) {
-  let stringNumber: string = num.toString();
-  let threeTimesNumber: string = stringNumber.repeat(3);
-  let intNumber: number = parseInt(threeTimesNumber);
+  let stringNumber: string = num.toString()
+  let threeTimesNumber: string = stringNumber.repeat(3)
+  let intNumber: number = parseInt(threeTimesNumber)
 
-  return num + intNumber;
+  return num + intNumber
 }
