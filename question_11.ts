43fb3ebbc005a78b2a31faff2d8d47b612db5705
Joji <freedive.cebu30@gmail.com>
2022-06-03 15:13:48 +0800

    Update question_11.ts

--- question_11.ts
+++ question_11.ts
@@ -1,12 +1,12 @@
-console.log(calAbs(10, 5));
-console.log(calAbs(10, 13));
+console.log(calAbs(10, 5))
+console.log(calAbs(10, 13))
 // 5
 // 3
 
 
 function calAbs(number1: number, number2: number): number {
-  let answer: number = number1 - number2;
-  answer = answer < 0 ? Math.abs(answer) : answer;
+  let answer: number = number1 - number2
+  answer = answer < 0 ? Math.abs(answer) : answer
 
-  return answer;
+  return answer
 }
