9290c3cf7b5b9f0906416b443f75605ac863a537
Joji <freedive.cebu30@gmail.com>
2022-06-06 00:59:02 +0800

    Create question_38.ts

---
+++ question_38.ts
@@ -0,0 +1,20 @@
+let people: any = []
+let mapPeople1 = new Map<string, any>([["name", "Taro1"], ["age", 20], ["height", 180], ["weight", 60], ["country", "Japan"]])
+let mapPeople2 = new Map<string, any>([["name", "Taro2"], ["age", 30], ["height", 170], ["weight", 65]])
+let mapPeople3 = new Map<string, any>([["name", "Taro3"], ["age", 40], ["height", 160], ["weight", 70], ["country", "Taiwan"]])
+let mapPeople4 = new Map<string, any>([["name", "Taro4"], ["age", 50], ["height", 150], ["weight", 75], ["country", "Japan"]])
+
+people.push(mapPeople1)
+people.push(mapPeople2)
+people.push(mapPeople3)
+people.push(mapPeople4)
+
+let listPeople = []
+
+for (var person of people ) {
+  if (person.has("country")){
+    listPeople.push(person)
+  }
+}
+
+console.log(listPeople)
