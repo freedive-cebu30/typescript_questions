e13cd1f244743e4440792757869ecc2e25fbc31b
Joji <freedive.cebu30@gmail.com>
2022-06-06 22:39:21 +0800

    Update question_10.ts

--- question_10.ts
+++ question_10.ts
@@ -2,7 +2,7 @@ console.log(cal(3))
 console.log(cal(4))
 
 
-function cal(num: number) {
+function cal(num: number): number {
   let stringNumber: string = num.toString()
   let threeTimesNumber: string = stringNumber.repeat(3)
   let intNumber: number = parseInt(threeTimesNumber)
